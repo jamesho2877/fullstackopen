ab6509e97d8d7651825926d4b3abfad51c517a9b
James Ho <hoangphuc.ho@frosmo.com>
2022-10-03 23:00:40 +0300

    2.1: Course information step6

---
+++ part_02/course_information/src/App.js
@@ -0,0 +1,67 @@
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return parts.map((section) => (
+    <Part
+      key={section.name}
+      name={section.name}
+      exercises={section.exercises}
+    />
+  ));
+};
+
+const Total = ({ courseTotal }) => {
+  return <p>Number of exercises {courseTotal}</p>;
+};
+
+const Course = ({ course }) => {
+  const courseTotal = course.parts.reduce((acc, course) => {
+    return (acc += course.exercises);
+  }, 0);
+
+  return (
+    <>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total courseTotal={courseTotal} /> */}
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14,
+      }
+    ]
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
