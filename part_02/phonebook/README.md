0109b795b437f0bad4eaaba9bba41efba229e440
James Ho <hoangphuc.ho@frosmo.com>
2022-10-04 18:05:43 +0300

    2.9*: The Phonebook Step4

--- part_02/phonebook/README.md
+++ part_02/phonebook/README.md
@@ -7,3 +7,5 @@ The following exercises can be found in this directory
 ### 2.7: The Phonebook Step2
 
 ### 2.8: The Phonebook Step3
+
+### 2.9*: The Phonebook Step4
--- part_02/phonebook/src/App.js
+++ part_02/phonebook/src/App.js
@@ -5,9 +5,15 @@ const Header = ({ text }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterKeyword, setFilterKeyword] = useState("");
 
   const handleNameChange = (e) => {
     const name = e.target.value;
@@ -17,6 +23,11 @@ const App = () => {
   const handleNumberChange = (e) => {
     const number = e.target.value;
     setNewNumber(number);
+  };
+
+  const handleFilterChange = (e) => {
+    const keyword = e.target.value;
+    setFilterKeyword(keyword);
   };
 
   const handleAddPerson = (e) => {
@@ -36,13 +47,21 @@ const App = () => {
     setNewNumber("");
   };
 
-  const nameListDOM = persons.map(person => (
+  const filteredPersons = filterKeyword.length > 0
+    ? persons.filter(person => person.name.toLowerCase().includes(filterKeyword.toLowerCase()))
+    : persons;
+  const nameListDOM = filteredPersons.map(person => (
     <div key={person.name}>{person.name} {person.number}</div>
   ));
 
   return (
     <div>
       <Header text="Phonebook" />
+      <div>
+        filter shown with: <input value={filterKeyword} onChange={handleFilterChange}/>
+      </div>
+
+      <Header text="add a new" />
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -54,7 +73,8 @@ const App = () => {
           <button type="submit" onClick={handleAddPerson}>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+
+      <Header text="Numbers" />
       {nameListDOM}
     </div>
   );
