77a9caaad95be7105aacda935a6dd4a4dbdb3743
James Ho <hoangphuc.ho@frosmo.com>
2022-10-07 15:38:37 +0300

    4.1 Blog list, step1

---
+++ part_04/blog_list/README.md
@@ -0,0 +1,5 @@
+## Exercise
+
+The following exercises can be found in this directory
+
+### 4.1 Blog list, step1
---
+++ part_04/blog_list/utils/config.js
@@ -0,0 +1,8 @@
+require("dotenv").config({ path: "./.env.local" });
+
+const { MONGODB_URI, PORT } = process.env;
+
+module.exports = {
+  MONGODB_URI,
+  PORT,
+};
