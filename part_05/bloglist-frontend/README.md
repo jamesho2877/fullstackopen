7a4f65515ad7fa8215ed38417bec2f77f2e095fd
James Ho <hoangphuc.ho@frosmo.com>
2022-10-11 17:33:10 +0300

    5.2: bloglist frontend, step2

--- part_05/bloglist-frontend/README.md
+++ part_05/bloglist-frontend/README.md
@@ -3,3 +3,5 @@
 The following exercises can be found in this directory
 
 ### 5.1: bloglist frontend, step1
+
+### 5.2: bloglist frontend, step2
--- part_05/bloglist-frontend/src/App.js
+++ part_05/bloglist-frontend/src/App.js
@@ -63,6 +63,11 @@ const App = () => {
     }
   };
 
+  const handleLogout = (event) => {
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setUser(null);
+  };
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -91,7 +96,7 @@ const App = () => {
     <>
       <h2>blogs</h2>
 
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
 
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
